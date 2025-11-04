1b567166ba8aa2af4c1349418b57c33411f198e0
Uncle Scientist <unclescientist@gmail.com>
2025-11-04 02:43:43 -0700

    Song of Ducks and Dragons, Quest 1, Parts 2 and 3

--- solutions/quest01/src/main.rs
+++ solutions/quest01/src/main.rs
@@ -3,7 +3,6 @@ fn main() {
     let names = lines[0].split(',').collect::<Vec<_>>();
     let inst = lines[1].split(',').collect::<Vec<_>>();
 
-    println!("names = {names:?}");
     let mut index = 0i64;
     for i in inst {
         let dist = i[1..].parse::<i64>().unwrap();
@@ -14,4 +13,38 @@ fn main() {
         }
     }
     println!("part 1 = {}", names[index as usize]);
+
+    // let lines = aoclib::read_lines("input/test_1.txt");
+    let lines = aoclib::read_lines("input/everybody_codes_e2025_q01_p2.txt");
+    let names = lines[0].split(',').collect::<Vec<_>>();
+    let inst = lines[1].split(',').collect::<Vec<_>>();
+
+    let mut index = 0i64;
+    for i in inst {
+        let dist = i[1..].parse::<i64>().unwrap();
+        if i.starts_with('R') {
+            index += dist;
+        } else if i.starts_with('L') {
+            index -= dist;
+        }
+    }
+    println!(
+        "part 2 = {}",
+        names[index.rem_euclid(names.len() as i64) as usize]
+    );
+
+    // let lines = aoclib::read_lines("input/test_1.txt");
+    let lines = aoclib::read_lines("input/everybody_codes_e2025_q01_p3.txt");
+    let mut names = lines[0].split(',').collect::<Vec<_>>();
+    let len = names.len() as i64;
+    let inst = lines[1].split(',').collect::<Vec<_>>();
+    for i in inst {
+        let dist = i[1..].parse::<i64>().unwrap();
+        if i.starts_with('R') {
+            names.swap(0, (dist % len) as usize);
+        } else {
+            names.swap(0, (-dist).rem_euclid(len) as usize);
+        }
+    }
+    println!("part 3 = {}", names[0]);
 }
