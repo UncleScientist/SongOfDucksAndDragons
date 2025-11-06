bda55b977c83a6135bd17513b12c5f0a775ca82b
Uncle Scientist <unclescientist@gmail.com>
2025-11-05 18:10:03 -0600

    Quest 3, Part 1

--- solutions/quest03/src/main.rs
+++ solutions/quest03/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    // let lines = aoclib::read_lines("input/test_1.txt");
+    let lines = aoclib::read_lines("input/everybody_codes_e2025_q03_p1.txt");
+    let mut nums = lines[0]
+        .split(',')
+        .map(|num| num.parse::<i64>().unwrap())
+        .collect::<Vec<_>>();
+    nums.sort();
+    let mut cur = nums[0] - 1;
+    let mut total = 0;
+    for n in nums {
+        if cur != n {
+            total += n;
+            cur = n;
+        }
+    }
+    println!("part 1 = {total}");
 }
