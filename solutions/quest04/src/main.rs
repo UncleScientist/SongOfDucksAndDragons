1e354f08147be3509c4c97d0dbaf8a644b3fef6b
Uncle Scientist <unclescientist@gmail.com>
2025-11-06 18:46:39 -0600

    Quest 4, Part 3

--- solutions/quest04/src/main.rs
+++ solutions/quest04/src/main.rs
@@ -17,6 +17,28 @@ fn main() {
         "part 2 = {}",
         (0.99999 + 10000000000000.0 / calc_gear(&gears)).floor()
     );
+
+    let lines = aoclib::read_lines("input/everybody_codes_e2025_q04_p3.txt");
+    // let lines = aoclib::read_lines("input/test_3.txt");
+    let first = lines[0].parse::<f64>().unwrap();
+    let mapping = lines[1..lines.len() - 1]
+        .iter()
+        .map(|line| {
+            let (left, right) = line.split_once('|').unwrap();
+            (left.parse::<f64>().unwrap(), right.parse::<f64>().unwrap())
+        })
+        .collect::<Vec<_>>();
+    let last = lines[lines.len() - 1].parse::<f64>().unwrap();
+    let mut ratio = first;
+    let mut turns = 100.0;
+    for (left, right) in mapping {
+        ratio /= left;
+        turns *= ratio;
+        println!("({left}|{right}) {ratio} -> turns = {turns}");
+        ratio = right;
+    }
+    ratio /= last;
+    println!("part 3 = {}", (turns * ratio).floor());
 }
 
 fn calc_gear(ratios: &[f64]) -> f64 {
