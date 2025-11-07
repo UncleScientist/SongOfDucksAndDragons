2a8b92106e0d62b01c576b8ee0a542c8c7f5fe81
Uncle Scientist <unclescientist@gmail.com>
2025-11-07 17:47:03 -0600

    Quest 5, Part 1

--- solutions/quest05/src/main.rs
+++ solutions/quest05/src/main.rs
@@ -1,3 +1,46 @@
 fn main() {
-    println!("Hello, world!");
+    // let lines = aoclib::read_lines("input/test_1.txt");
+    let lines = aoclib::read_lines("input/everybody_codes_e2025_q05_p1.txt");
+    let (_, nums) = lines[0].split_once(':').unwrap();
+    let nums = nums
+        .split(',')
+        .map(|num| num.parse::<usize>().unwrap())
+        .collect::<Vec<_>>();
+    let mut sword: Vec<Element> = Vec::new();
+    let mut part1 = String::new();
+    for n in nums {
+        let mut found = false;
+        for s in &mut sword {
+            if n < s.spine && s.left.is_none() {
+                s.left = Some(n);
+                found = true;
+                break;
+            } else if n > s.spine && s.right.is_none() {
+                s.right = Some(n);
+                found = true;
+                break;
+            }
+        }
+        if !found {
+            sword.push(Element::add(n));
+            part1.push_str(format!("{n}").as_str());
+        }
+    }
+    println!("part 1 = {part1}");
+}
+
+#[derive(Debug, Default)]
+struct Element {
+    left: Option<usize>,
+    spine: usize,
+    right: Option<usize>,
+}
+
+impl Element {
+    fn add(spine: usize) -> Self {
+        Self {
+            spine,
+            ..Self::default()
+        }
+    }
 }
