dc81dec68e3d5a55a6334ad8b61002c8481898a9
Uncle Scientist <unclescientist@gmail.com>
2025-11-06 18:23:57 -0600

    Quest 4, Part 1

--- solutions/quest04/src/main.rs
+++ solutions/quest04/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    /*
+    println!("{}", calc_gear(&[128.0, 64.0, 32.0, 16.0, 8.0]));
+    println!(
+        "{}",
+        (2025.0 * calc_gear(&[102.0, 75.0, 50.0, 35.0, 13.0])).floor()
+    );
+    */
+    let lines: Vec<f64> = aoclib::read_numbers("input/everybody_codes_e2025_q04_p1.txt");
+    println!("part 1 = {}", (calc_gear(&lines) * 2025.0).floor());
+}
+
+fn calc_gear(ratios: &[f64]) -> f64 {
+    let mut result: f64 = 1.0;
+
+    for pair in ratios.windows(2) {
+        result *= pair[0] / pair[1];
+    }
+
+    result
 }
