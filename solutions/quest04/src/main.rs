5cb2994ec78c694c3483a198e5c92ea8038fd8f0
Uncle Scientist <unclescientist@gmail.com>
2025-11-06 18:29:17 -0600

    Quest 4, Part 2

--- solutions/quest04/src/main.rs
+++ solutions/quest04/src/main.rs
@@ -1,13 +1,22 @@
 fn main() {
     /*
-    println!("{}", calc_gear(&[128.0, 64.0, 32.0, 16.0, 8.0]));
     println!(
         "{}",
-        (2025.0 * calc_gear(&[102.0, 75.0, 50.0, 35.0, 13.0])).floor()
+        (0.9999 + 10000000000000.0 / calc_gear(&[128.0, 64.0, 32.0, 16.0, 8.0])).floor()
+    );
+    println!(
+        "{}",
+        (0.99999 + 10000000000000.0 / calc_gear(&[102.0, 75.0, 50.0, 35.0, 13.0])).floor()
     );
     */
-    let lines: Vec<f64> = aoclib::read_numbers("input/everybody_codes_e2025_q04_p1.txt");
-    println!("part 1 = {}", (calc_gear(&lines) * 2025.0).floor());
+    let gears: Vec<f64> = aoclib::read_numbers("input/everybody_codes_e2025_q04_p1.txt");
+    println!("part 1 = {}", (calc_gear(&gears) * 2025.0).floor());
+
+    let gears: Vec<f64> = aoclib::read_numbers("input/everybody_codes_e2025_q04_p2.txt");
+    println!(
+        "part 2 = {}",
+        (0.99999 + 10000000000000.0 / calc_gear(&gears)).floor()
+    );
 }
 
 fn calc_gear(ratios: &[f64]) -> f64 {
