fd76de20328257371b88fc15ec7cc72e9f95b8c3
Uncle Scientist <unclescientist@gmail.com>
2025-11-11 07:07:20 -0500

    Quest 6, Part 1

--- solutions/quest06/src/main.rs
+++ solutions/quest06/src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    let lines = aoclib::read_lines("input/everybody_codes_e2025_q06_p1.txt");
+    println!("part 1 = {}", count_swordfighting(&lines[0]));
+}
+
+fn count_swordfighting<S: AsRef<str>>(notes: S) -> usize {
+    let mut knight_count = 0;
+    let mut novice_count = 0;
+    for c in notes.as_ref().chars() {
+        match c {
+            'A' => knight_count += 1,
+            'a' => novice_count += knight_count,
+            _ => {}
+        }
+    }
+
+    novice_count
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example() {
+        assert_eq!(5, count_swordfighting("AaAaa"));
+    }
 }
