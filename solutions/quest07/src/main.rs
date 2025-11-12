828f5ac09f673efae3ed93a850a3b28b0bdb252c
Uncle Scientist <unclescientist@gmail.com>
2025-11-12 06:21:39 -0500

    Quest 7, Part 2

--- solutions/quest07/src/main.rs
+++ solutions/quest07/src/main.rs
@@ -1,19 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::{collections::{HashMap, HashSet}, path::Path};
 
 fn main() {
-    let lines = aoclib::read_lines("input/everybody_codes_e2025_q07_p1.txt");
-    // let lines = aoclib::read_lines("input/test_1.txt");
-    let names = lines[0].split(',').collect::<Vec<_>>();
-    let mut mapping = HashMap::<char, HashSet<char>>::new();
-    for map in &lines[1..] {
-        let (letter, list) = map.split_once(" > ").unwrap();
-        let list = list
-            .split(',')
-            .map(|ch| ch.chars().next().unwrap())
-            .collect::<HashSet<char>>();
-        mapping.insert(letter.chars().next().unwrap(), list);
-    }
-
+    let Rules {names, mapping} = Rules::read("input/everybody_codes_e2025_q07_p1.txt");
     for name in names {
         let mut cur_set: Option<HashSet<char>> = None;
         let mut success = true;
@@ -31,4 +19,47 @@ fn main() {
             break;
         }
     }
+
+    let Rules {names, mapping} = Rules::read("input/everybody_codes_e2025_q07_p2.txt");
+    let mut total = 0;
+    for (idx, name) in names.into_iter().enumerate() {
+        let mut cur_set: Option<HashSet<char>> = None;
+        let mut success = true;
+        for ch in name.chars() {
+            if let Some(set) = cur_set
+                && !set.contains(&ch)
+            {
+                success = false;
+                break;
+            }
+            cur_set = mapping.get(&ch).cloned();
+        }
+        if success {
+            total += idx + 1;
+        }
+    }
+    println!("part 2 = {total}");
 }
+
+struct Rules {
+    names: Vec<String>,
+    mapping: HashMap<char, HashSet<char>>,
+}
+
+impl Rules {
+    fn read<P: AsRef<Path>>(pathname: P) -> Self {
+        let lines = aoclib::read_lines(pathname);
+    let names = lines[0].split(',').map(|name| name.to_string()).collect::<Vec<_>>();
+    let mut mapping = HashMap::<char, HashSet<char>>::new();
+    for map in &lines[1..] {
+        let (letter, list) = map.split_once(" > ").unwrap();
+        let list = list
+            .split(',')
+            .map(|ch| ch.chars().next().unwrap())
+            .collect::<HashSet<char>>();
+        mapping.insert(letter.chars().next().unwrap(), list);
+    }
+    Self { names, mapping }
+    }
+}
+
