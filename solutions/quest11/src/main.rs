673f25cc615d077d7819d8cbe2d24ffadac58963
Uncle Scientist <unclescientist@gmail.com>
2025-11-18 06:48:27 -0500

    Quest 11, Part 2

--- solutions/quest11/src/main.rs
+++ solutions/quest11/src/main.rs
@@ -5,10 +5,16 @@ fn main() {
         .map(|line| line.parse().unwrap())
         .collect::<Vec<_>>();
     let mut flock = Flock::new(&columns);
-    for _ in 0..10 {
-        flock.round();
-    }
+    flock.do_rounds(10);
     println!("part 1 = {}", flock.checksum());
+
+    let lines = aoclib::read_lines("input/everybody_codes_e2025_q11_p2.txt");
+    let columns = lines
+        .iter()
+        .map(|line| line.parse().unwrap())
+        .collect::<Vec<_>>();
+    let mut flock = Flock::new(&columns);
+    println!("part 2 = {}", flock.rounds_until_balanced());
 }
 
 #[derive(Debug)]
@@ -25,7 +31,7 @@ impl Flock {
         }
     }
 
-    fn round(&mut self) {
+    fn round(&mut self) -> bool {
         match self.phase {
             Phase::First => {
                 let mut moved = false;
@@ -51,13 +57,30 @@ impl Flock {
                     }
                 }
                 if !moved {
-                    self.phase = Phase::Done
+                    self.phase = Phase::Done;
+                    return true;
                 }
             }
             Phase::Done => {
                 println!("done!");
             }
         }
+
+        false
+    }
+
+    fn do_rounds(&mut self, rounds: usize) {
+        for _ in 0..rounds {
+            self.round();
+        }
+    }
+
+    fn rounds_until_balanced(&mut self) -> usize {
+        let mut count = 0;
+        while !self.round() {
+            count += 1;
+        }
+        count
     }
 
     fn checksum(&self) -> usize {
@@ -68,7 +91,7 @@ impl Flock {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Phase {
     First,
     Second,
@@ -83,9 +106,14 @@ mod tests {
     fn test_example1() {
         let columns = [9, 1, 1, 4, 9, 6];
         let mut flock = Flock::new(&columns);
-        for _ in 0..10 {
-            flock.round()
-        }
+        flock.do_rounds(10);
         assert_eq!(109, flock.checksum());
+    }
+
+    #[test]
+    fn test_example2_2() {
+        let columns = [805, 706, 179, 48, 158, 150, 232, 885, 598, 524, 423];
+        let mut flock = Flock::new(&columns);
+        assert_eq!(1579, flock.rounds_until_balanced());
     }
 }
