ae1a6d57c44b7028d89118aa77b994e31bced4ee
Uncle Scientist <unclescientist@gmail.com>
2025-11-19 07:40:01 -0500

    Quest 12, Part 3

--- solutions/quest12/src/main.rs
+++ solutions/quest12/src/main.rs
@@ -3,17 +3,24 @@ use std::collections::HashSet;
 fn main() {
     let lines = aoclib::read_lines("input/everybody_codes_e2025_q12_p1.txt");
     let grid = Grid::new(&lines);
-    println!("part 1 = {}", grid.explode(&[(0, 0)]));
+    println!("part 1 = {}", grid.explode(&[(0, 0)]).len());
 
     let lines = aoclib::read_lines("input/everybody_codes_e2025_q12_p2.txt");
     let grid = Grid::new(&lines);
     println!(
         "part 2 = {}",
         grid.explode(&[(0, 0), (grid.height - 1, grid.width - 1)])
+            .len()
     );
+
+    let lines = aoclib::read_lines("input/everybody_codes_e2025_q12_p3.txt");
+    let grid = Grid::new(&lines);
+    println!("part 3 = {}", grid.triple_explosion());
 }
 
-#[derive(Debug)]
+type BarrelSet = HashSet<(isize, isize)>;
+
+#[derive(Debug, Clone)]
 struct Grid {
     width: isize,
     height: isize,
@@ -37,13 +44,17 @@ impl Grid {
         }
     }
 
-    fn explode(&self, start: &[(isize, isize)]) -> usize {
+    fn explode(&self, start: &[(isize, isize)]) -> BarrelSet {
         let mut stack = Vec::from(start);
-        let mut visited = HashSet::<(isize, isize)>::new();
+        let mut visited = BarrelSet::new();
 
         while let Some(pos) = stack.pop() {
+            let cur_value = self.barrels[pos.0 as usize][pos.1 as usize];
+            if cur_value == 10 {
+                continue;
+            }
+
             if visited.insert(pos) {
-                let cur_value = self.barrels[pos.0 as usize][pos.1 as usize];
                 for dir in [(-1, 0), (0, 1), (1, 0), (0, -1)] {
                     let new_pos = (pos.0 + dir.0, pos.1 + dir.1);
                     if new_pos.0 < 0
@@ -61,7 +72,31 @@ impl Grid {
             }
         }
 
-        visited.len()
+        visited
+    }
+
+    fn triple_explosion(&self) -> usize {
+        let mut cur_grid = self.clone();
+        let mut total = 0;
+
+        for _ in 0..3 {
+            let mut most = BarrelSet::new();
+            for row in 0..cur_grid.height {
+                for col in 0..cur_grid.width {
+                    let cur = cur_grid.explode(&[(row, col)]);
+
+                    if cur.len() > most.len() {
+                        most = cur;
+                    }
+                }
+            }
+
+            total += most.len();
+            for barrel in most {
+                cur_grid.barrels[barrel.0 as usize][barrel.1 as usize] = 10;
+            }
+        }
+        total
     }
 }
 
@@ -73,7 +108,7 @@ mod tests {
     fn test_example1() {
         let lines = ["989601", "857782", "746543", "766789"];
         let grid = Grid::new(&lines);
-        assert_eq!(16, grid.explode(&[(0, 0)]));
+        assert_eq!(16, grid.explode(&[(0, 0)]).len());
     }
 
     #[test]
@@ -94,6 +129,33 @@ mod tests {
         assert_eq!(
             58,
             grid.explode(&[(0, 0), (grid.height - 1, grid.width - 1)])
+                .len()
         );
+    }
+
+    #[test]
+    fn test_example3_1() {
+        let lines = ["5411", "3362", "5235", "3112"];
+        let grid = Grid::new(&lines);
+        assert_eq!(14, grid.triple_explosion());
+    }
+
+    #[test]
+    fn test_example3_2() {
+        let lines = [
+            "41951111131882511179",
+            "32112222211518122215",
+            "31223333322115122219",
+            "31234444432147511128",
+            "91223333322176121892",
+            "61112222211166431583",
+            "14661111166111111746",
+            "11111119142122222177",
+            "41222118881233333219",
+            "71222127839122222196",
+            "56111126279711111517",
+        ];
+        let grid = Grid::new(&lines);
+        assert_eq!(136, grid.triple_explosion());
     }
 }
